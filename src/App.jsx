04100d08497e2f498847b1d336023aa22ca1160d
Vicky Soni <murlimanoharsoni4@gmail.com>
2022-10-13 10:06:36 +0530

    added nanoid as dependency
    generates unique keys

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import { useState } from "react";
+import React from "react";
 import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello World!</h1>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>Hello World!</h1>
+        </div>
+    );
 }
 
 export default App;
