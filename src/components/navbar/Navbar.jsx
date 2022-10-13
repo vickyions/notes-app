d070e0975b98928fc28c0950e39d6380f2b4fca0
Vicky Soni <murlimanoharsoni4@gmail.com>
2022-10-13 18:44:39 +0530

    added switch component for theme functionality still have to create a switch theme utility function/usecontext

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import Search from '../search/Search';
 import Layouts from '../layouts/Layouts';
-//import SwitchTheme from '../switch/SwitchTheme';
+import Switch from '../switch/Switch';
+//import switchTheme from //somewher which will change the global theme value;
 
 export default function Navbar() {
     
+    //temperary change with global switchTheme function
+    function switchTheme(isOn) {console.log(isOn)};
+
     return (
         <nav className="nav light">
             <h1>Notes</h1>
             <Search />
             <Layouts />
-            {/*<SwitchTheme />*/}
+            <Switch options={["Light", "Dark"]} toggleFunction={switchTheme}/>
         </nav>
     );
 }
