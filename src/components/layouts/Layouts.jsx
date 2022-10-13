009d732de87c0f77c61be0abc58a85618955bec9
Vicky Soni <murlimanoharsoni4@gmail.com>
2022-10-13 11:40:15 +0530

    added the Layout buttons and partial toggle layout function

---
+++ src/components/layouts/Layouts.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGrip, faGripLines } from "@fortawesome/free-solid-svg-icons";
+
+export default function Layouts() {
+    
+    function toggleLayouts(layout, event) {
+        switch (layout) {
+            case "grid-layouts":
+                //handle changing the class of note container
+                break;
+            case "list-layouts":
+                break;
+        }
+
+        /*TODO*/
+
+    }
+
+    return (
+        <div className="layouts light">
+            <div className="grid-layout" onClick={(e) => toggleLayouts("gird-layout", e)}>
+                <FontAwesomeIcon icon={faGrip} />
+            </div>
+            <div className="list-layout" onClick={(e) => toggleLayouts("list-layout", e)}>
+                <FontAwesomeIcon icon={faGripLines} />
+            </div>
+        </div>
+    );
+}
