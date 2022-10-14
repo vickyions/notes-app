615747e1c6532faa53e1a8dfb814c2c0511cb520
Vicky Soni <murlimanoharsoni4@gmail.com>
2022-10-14 23:47:33 +0530

    implimenting local storage for notes array and a reducer for notes

--- src/components/notes/Notes.jsx
+++ src/components/notes/Notes.jsx
@@ -1,8 +1,27 @@
-import React, { useReducer } from "react";
+import React, { useEffect, useReducer } from "react";
+
+const NOTES_STR_KEY = "notes.app";
+
+const action = {
+
+}
+
+const reducer(state, action) {
+
+}
 
 export default function Notes() {
     //create a reducer which will always return a filtered notes
-    const [notes] = useReducer(reducer, {});
+    const [notes, dispatch] = useReducer(
+        reducer,
+        (() => {
+            JSON.parse(localStorage.getItem(NOTES_STR_KEY));
+        }) || []
+    );
+
+    useEffect(() => {
+        localStorage.setItem(NOTES_STR_KEY, JSON.stringify(notes));
+    }, [notes]);
 
     return (
         <div className="notes-container">
