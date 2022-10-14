ecf2f3990d36eb06ee77eac4eeda9a4ea55e0362
Vicky Soni <murlimanoharsoni4@gmail.com>
2022-10-14 22:25:30 +0530

    corrected onClick toggle handling

--- src/components/switch/Switch.jsx
+++ src/components/switch/Switch.jsx
@@ -3,19 +3,19 @@ import React, {useState} from 'react';
 import './Switch.css';
 
 export default function Switch(props) {
-    //takes a function and toggles between them should also recieve a togglefunction which will be called to perform a specific action
-    const {options, toggleFunction} = props;
+    //recieves a theme toggles from context
+    //will call it for every button click
+    const {options} = props;
     const [optOne, optTwo] = options;
 
     const [isOn, setIsOn] = useState(false);
 
     function onClickHandler() {
         setIsOn(prevIsOn => !prevIsOn);
-        toggleFunction(isOn);
     }
 
     return (
-        <div className="switch-container" onClick={() => {onClickHandler}}>
+        <div className="switch-container" onClick={onClickHandler}>
             <div className={`switch-left ${!isOn && "active"}`} >{optOne}</div>
             <div className={`switch switch-${isOn ? "on" : "off"}`}>
                 <div className="switch-circle"></div>
