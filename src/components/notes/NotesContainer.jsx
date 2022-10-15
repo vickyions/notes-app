53a9e264f399d6df1f869fdbeb9d6ba1091f0ff0
Vicky Soni <murlimanoharsoni4@gmail.com>
2022-10-15 14:26:01 +0530

    corrected some comments in reducer functionality

--- src/components/notes/NotesContainer.jsx
+++ src/components/notes/NotesContainer.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useReducer } from "react";
 
 const NOTES_STR_KEY = "notes.app";
+/*notes [notesObj]
 /*notesObj {
     content,
     backgroundColor,
@@ -14,29 +15,41 @@ const NOTES_STR_KEY = "notes.app";
 const ACTION = {
     ADD_NOTE: "addNote",
     DELETE_NOTE: "deleteNote",
-    GET_NOTES: "getNotes", //will call a function which takes a query for filtering
+    ADD_TAG: "addTag",
+    DELETE_TAG: "deleteTag",
+    TOGGLE_ARCHIVE: "togglearchive",
+    TOGGLE_TRASH: "tpggleTrash",
+    TOGGLE_PINNED: "togglePinned",
 };
 
 function reducer(prevState, action) {
     /*action {
         type: ACTION.<keyword>,
-        query: /*for returning a filtered notes array *//*
     }*/
     switch (action.type) {
         case ACTION.ADD_NOTE:
             break;
         case ACTION.DELETE_NOTE:
             break;
-        case ACTION.GET_NOTES:
-            //will be a filter function which takes a query also from action.query
+        case ACTION.ADD_TAG:
+            break;
+        case ACTION.DELETE_TAG:
+            break;
+        case ACTION.TOGGLE_ARCHIVE:
+            break;
+        case ACTION.TOGGLE_TRASH:
+            break;
+        case ACTION.TOGGLE_PINNED:
             break;
         default:
             return prevState;
     }
 }
 
-export default function Notes() {
-    //create a reducer which will always return a filtered notes
+//write several function to filter notes will be used to map around
+//write in a utils js
+
+export default function NotesContainer() {
     const [notes, dispatch] = useReducer(
         reducer,
         (() => {
