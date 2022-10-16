8354758c5261e89e639b6e5b10cf7bd51c458cc8
Vicky Soni <murlimanoharsoni4@gmail.com>
2022-10-17 01:58:40 +0530

    corrected import

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import "./App.css";
 import Navbar from './components/navbar/Navbar';
+import NotesContainer from './components/notes/NotesContainer'
 
 function App() {
     return (
         <div className="App">
             <Navbar />
             <h1>Hello World!</h1>
+            <NotesContainer />
         </div>
     );
 }
