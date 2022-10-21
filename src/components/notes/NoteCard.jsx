bddc76b816768add0b1e2849a04d7d1f29b3a2b8
Vicky Soni <murlimanoharsoni4@gmail.com>
2022-10-21 13:49:28 +0530

    Added some action button functionlity for pin trash archive toggle

--- src/components/notes/NoteCard.jsx
+++ src/components/notes/NoteCard.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import "./NoteCard.css";
+import { ACTION } from "./NotesContainer";
 
 /*notes [notesObj]
 /*notesObj {
@@ -12,11 +14,38 @@ import React from "react";
 } */
 
 export default function NoteCard(props) {
-    const {note, dispatch} = props;
+    const { note, dispatch } = props;
 
     return (
         <div className="note-card">
             <h1>{note.content}</h1>
+            <p>{JSON.stringify(note)}</p>
+            <div className="note-card-actions">
+                <button
+                    type="button"
+                    onClick={() =>
+                        dispatch({ type: ACTION.TOGGLE_ARCHIVE, id: note.id })
+                    }
+                >
+                    Archive
+                </button>
+                <button
+                    type="button"
+                    onClick={() =>
+                        dispatch({ type: ACTION.TOGGLE_TRASH, id: note.id })
+                    }
+                >
+                    Trash
+                </button>
+                <button
+                    type="button"
+                    onClick={() =>
+                        dispatch({ type: ACTION.TOGGLE_PINNED, id: note.id })
+                    }
+                >
+                    Pin
+                </button>
+            </div>
         </div>
     );
 }
