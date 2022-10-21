67d5012564f40a71405306f09f2a2faa4241a59b
Vicky Soni <murlimanoharsoni4@gmail.com>
2022-10-21 16:24:59 +0530

    clearing and focusing input box after every new note addition

--- src/components/notes/InputNote.jsx
+++ src/components/notes/InputNote.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { useRef } from "react";
 import { ACTION } from "./NotesContainer";
 
 /*notes [notesObj]
@@ -17,6 +18,7 @@ export default function InputNote(props) {
     const [noteContent, setNoteContent] = useState("");
 
     const { dispatch } = props;
+    const inputNoteRef = useRef(null);
 
     return (
         <div className="input-note-container">
@@ -25,14 +27,18 @@ export default function InputNote(props) {
                 placeholder="Input note"
                 value={noteContent}
                 onChange={(event) => setNoteContent(event.target.value)}
+                ref={inputNoteRef}
             />
             <button
                 type="button"
-                onClick={() =>
+                onClick={() =>{
                     dispatch({
                         type: ACTION.ADD_NOTE,
                         noteContent: noteContent,
-                    })
+                    });
+                    setNoteContent("");
+                    inputNoteRef.current.focus();
+                }
                 }
             >Add Note</button>
         </div>
